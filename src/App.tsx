d41ff13b09ece06922f8cd34445e65b3a95366f8
ren-joey <tsuyiren@gmail.com>
2022-01-04 17:11:14 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import './App.scss';
 import Web3Component from './views/Web3Component';
+import logo from './logo.svg';
+import './App.scss';
 
 const App = () => (
     <div className="App">
-        <Web3Component />
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+                <Web3Component />
+            </a>
+        </header>
     </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,1 @@
 /// <reference types="react-scripts" />
-declare module 'web3/dist/web3.min.js'
