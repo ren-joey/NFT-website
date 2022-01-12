e5a254175efa02c8c77a89ec86fe73b2e1be08cf
ren-joey <tsuyiren@gmail.com>
2022-01-12 09:38:05 +0800

    [開發] 框架調整完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import Web3Component from './views/Web3Component';
-import logo from './logo.svg';
-import './App.scss';
+import FrontPage from 'src/views/FrontPage';
+// import Web3Component from './views/Web3Component';
+// import 'src/styles/reset.css';
+import 'src/styles/index.scss';
 
 const App = () => (
     <div className="App">
-        <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-                <Web3Component />
-            </a>
-        </header>
+        <FrontPage />
+        {/* <Web3Component /> */}
     </div>
 );
 
