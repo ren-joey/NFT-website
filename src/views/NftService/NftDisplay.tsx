96be56cfe0665c279c67fea340ce1e7ea81c4df3
ren-joey <tsuyiren@gmail.com>
2022-02-25 18:49:36 +0800

    [開發] 代碼整理

--- src/views/NftService/NftDisplay.tsx
+++ src/views/NftService/NftDisplay.tsx
@@ -27,6 +27,44 @@ const NftDisplay = () => {
         setNfts(_nfts as INft[]);
     }, [data]);
 
+    const nftWithMetadata = (nft: INft) => (<div className={`${whiteCard} flex-col`}>
+        <img src={nft.metadata.image} alt={nft.metadata.name} />
+        <h3 className={`${blackTitle} mt-5`}>
+            {nft.metadata.name}
+        </h3>
+        <div className={blackDescription}>
+            {nft.metadata.description}
+        </div>
+        <p className="text-xs text-gray-400">
+            ID: {nft.token_id}
+        </p>
+        <div className="mt-2">
+            <button
+                className={cyanBtn}
+                onClick={() => refreshNft(nft)}
+            >
+                Refresh Metadata
+            </button>
+        </div>
+    </div>);
+
+    const nftWithoutMetadata = (nft: INft) => (
+        <div className={`${whiteCard} flex-col`} style={{
+            minWidth: '300px'
+        }}>
+            <p className={blackTitle}>
+                此 NFT METADATA 遺失<br />
+                請聯繫 VBC 團隊取得協助<br />
+            </p>
+            <p className={blackDescription}>
+                請放心，您的 NFT 已 mint 成功
+            </p>
+            <p className="text-xs text-gray-400">
+                ID: {nft.token_id}
+            </p>
+        </div>
+    );
+
     return (
         <div className="flex flex-wrap flex- mx-5">
             {
@@ -35,35 +73,9 @@ const NftDisplay = () => {
                         <div className="flex-1 px-2" key={idx}>
                             {
                                 nft.metadata ? (
-                                    <div className={`${whiteCard} flex-col`}>
-                                        <img src={nft.metadata.image} alt={nft.metadata.name} />
-                                        <h3 className={`${blackTitle} mt-5`}>
-                                            {nft.metadata.name}
-                                        </h3>
-                                        <div className={blackDescription}>
-                                            {nft.metadata.description}
-                                        </div>
-                                        <div className="mt-2">
-                                            <button
-                                                className={cyanBtn}
-                                                onClick={() => refreshNft(nft)}
-                                            >
-                                                Refresh Metadata
-                                            </button>
-                                        </div>
-                                    </div>
+                                    nftWithMetadata(nft)
                                 ) : (
-                                    <div className={`${whiteCard} flex-col`} style={{
-                                        minWidth: '300px'
-                                    }}>
-                                        <p className={blackTitle}>
-                                            此 NFT METADATA 遺失<br />
-                                            請聯繫 VBC 團隊取得協助<br />
-                                        </p>
-                                        <p className={blackDescription}>
-                                            (請放心，您的 NFT 已 mint 成功)
-                                        </p>
-                                    </div>
+                                    nftWithoutMetadata(nft)
                                 )
                             }
                         </div>
