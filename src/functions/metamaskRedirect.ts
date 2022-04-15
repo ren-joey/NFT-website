3afdb031a1af01a3ff4706455c89fce64af37807
ren-joey <tsuyiren@gmail.com>
2022-04-15 16:33:31 +0800

    [開發] 修正正規表達式

--- src/functions/metamaskRedirect.ts
+++ src/functions/metamaskRedirect.ts
@@ -1,7 +1,7 @@
 const metamaskRedirect = (url: string = window.location.href) => {
     const cUrl = new URL(url);
     cUrl.searchParams.set('auto-login', 'true');
-    const fixedUrl = cUrl.toString().replace(/^htt(p|s):\/\//g, '');
+    const fixedUrl = cUrl.toString().replace(/^htt(p|ps):\/\//g, '');
     window.open('https://metamask.app.link/dapp/' + fixedUrl);
 };
 
