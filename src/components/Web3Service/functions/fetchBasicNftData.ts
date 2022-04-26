44464a1901afd6421fdbb3e88df0a3ccdc9d149d
ren-joey <tsuyiren@gmail.com>
2022-04-26 12:00:09 +0800

    [更新] issue-57 update

--- src/components/Web3Service/functions/fetchBasicNftData.ts
+++ src/components/Web3Service/functions/fetchBasicNftData.ts
@@ -14,15 +14,23 @@ const fetchBasicNftData = (
     { nfts, account, fetch }: Props
 ) => new Promise<BasicNft[]>((allDone) => {
     const promises: Promise<BasicNft>[] = [];
+
+    // 所有 token_id 巡訪
     nfts.forEach((tokenId) => {
         promises.push(
             new Promise<BasicNft>((resolve) => {
+
+                // 利用 Smart Contract 的 tokenURI method
+                // 取得所有 token 的 metadata url
                 fetchContractVariable<ContractResponse>({
                     paramName: 'tokenURI',
                     params: { tokenId },
                     fetch
                 }).then((uri) => {
                     if (typeof uri === 'string') {
+
+                        // 將取得的 metadata url 進行 get
+                        // 得到的 json response 即為該 token_id 的 metadata 內容
                         axios.get(uri).then((res) => {
                             resolve({
                                 token_id: tokenId.toString(),
