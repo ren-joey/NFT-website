5f6eedb21923ecf99acc839dc1a5f05d3d6b2692
ren-joey <tsuyiren@gmail.com>
2022-06-21 12:28:03 +0800

    [修正] 語法錯誤修正

--- src/@types/basicVariable.ts
+++ src/@types/basicVariable.ts
@@ -3,7 +3,7 @@ import { BigNumber } from "ethers";
 export type Nullable = null|number;
 export type NullableBigNumber = null|BigNumber;
 export type NullableString = null|string;
-export type SocialIconName = 'twitter'|'opensea'|'discord'|'ig'|'promotion_1';
+export type SocialIconName = 'twitter'|'opensea'|'discord'|'ig'|'promotion';
 export type BtnColorList = 'default'|'gray'|'orange';
 export type DeviceString = 'desktop'|'phone';
 export type LangString = 'ZH_TW'|'ZH_CN'|'EN';
