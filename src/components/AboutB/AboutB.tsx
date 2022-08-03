07c6432c401c6e6e846da42b5db3b1a7a6256ce1
ren-joey <tsuyiren@gmail.com>
2022-08-03 13:47:04 +0800

    [開發] 新增影片並修改 eslint 設定

--- src/components/AboutB/AboutB.tsx
+++ src/components/AboutB/AboutB.tsx
@@ -14,46 +14,49 @@ interface Props {
 
 const AboutB = ({ selectedLang }: Props) => {
     const { device } = useContext(EventContext);
-    const lang = useContext(LangContext);
     const skullIcon: React.CSSProperties = { backgroundImage: `url(${getResources('skull_icon')})` };
 
     return (
         <div className="about-b">
-            <div className="desc-area">
-                <div className="guide-line pc">
-                    <div id="aboutBLine" className="line"></div>
-                    <div className="skull" style={skullIcon}></div>
-                    <div className="star-container">
-                        <Star />
-                    </div>
-                </div>
-                <div id="aboutBContent" className="content">
-                    <div className="title-img" style={
-                        {backgroundImage: `url(${getResources('about_b')})`}
-                    }></div>
-                    <div className="desc pc" dangerouslySetInnerHTML={{__html: lang.ABOUT_B_DESC_PC}}></div>
-                    <div className="desc phone" dangerouslySetInnerHTML={{__html: lang.ABOUT_B_DESC}}></div>
-                </div>
+            <div className="iframe-container">
+                <iframe
+                    className="iframe"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/sSN81rcCQoA?autoplay=1&loop=1"
+                    title="Betamon awakening"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
             </div>
 
             {
                 device === 'phone' &&
-                    <div className="phone-skull" style={skullIcon}>
-                        <div id="aboutBPhoneLine" className={`phone-line ${selectedLang === 'EN' && 'en'}`}></div>
+                    <div className="phone-skull"
+                        style={skullIcon}>
+                        <div id="aboutBPhoneLine"
+                            className={`phone-line ${selectedLang === 'EN' && 'en'}`}></div>
                     </div>
             }
 
             {
                 device === 'desktop' ?
                     <div className="feature-card-area">
-                        <div id="featureCardRow1" className="feature-card-row">
+                        <div id="featureCardRow1"
+                            className="feature-card-row">
 
-                            <FeatureCard idx={1} selectedLang={selectedLang} />
-                            <FeatureCard idx={2} selectedLang={selectedLang} />
-                            <FeatureCard idx={3} line={false} selectedLang={selectedLang}/>
+                            <FeatureCard idx={1}
+                                selectedLang={selectedLang} />
+                            <FeatureCard idx={2}
+                                selectedLang={selectedLang} />
+                            <FeatureCard idx={3}
+                                line={false}
+                                selectedLang={selectedLang}/>
 
                             <div
-                                id="lineTurningAround" className="line-turning-around"
+                                id="lineTurningAround"
+                                className="line-turning-around"
                                 style={
                                     { backgroundImage: `url(${getResources('line_turning_around')})` }
                                 }
@@ -63,13 +66,17 @@ const AboutB = ({ selectedLang }: Props) => {
                                 </div>
                             </div>
                         </div>
-                        <div id="featureCardRow2" className="feature-card-row reverse">
+                        <div id="featureCardRow2"
+                            className="feature-card-row reverse">
                             <div className="aside">
                                 <BAlienSlider />
                             </div>
 
-                            <FeatureCard idx={4} line={false} selectedLang={selectedLang} />
-                            <FeatureCard idx={5} selectedLang={selectedLang} />
+                            <FeatureCard idx={4}
+                                line={false}
+                                selectedLang={selectedLang} />
+                            <FeatureCard idx={5}
+                                selectedLang={selectedLang} />
 
                             <div className="aside"></div>
                             <div
@@ -79,9 +86,10 @@ const AboutB = ({ selectedLang }: Props) => {
                                     { backgroundImage: `url(${getResources('line_turning_left')})` }
                                 }
                             >
-                                <div className="star-container" style={
-                                    { backgroundImage: `url(${getResources('glowing_star')})` }
-                                }></div>
+                                <div className="star-container"
+                                    style={
+                                        { backgroundImage: `url(${getResources('glowing_star')})` }
+                                    }></div>
                             </div>
                         </div>
                     </div>
@@ -90,21 +98,26 @@ const AboutB = ({ selectedLang }: Props) => {
                         <div className="feature-card-row">
                             <div className="feature-card-col space-left">
                                 <div id="featureCard1">
-                                    <FeatureCard idx={1} selectedLang={selectedLang} />
+                                    <FeatureCard idx={1}
+                                        selectedLang={selectedLang} />
                                 </div>
                                 <div id="featureCard3">
-                                    <FeatureCard idx={3} selectedLang={selectedLang} />
+                                    <FeatureCard idx={3}
+                                        selectedLang={selectedLang} />
                                 </div>
                                 <div id="featureCard5">
-                                    <FeatureCard idx={5} selectedLang={selectedLang} />
+                                    <FeatureCard idx={5}
+                                        selectedLang={selectedLang} />
                                 </div>
                             </div>
                             <div className="feature-card-col space-right">
                                 <div id="featureCard2">
-                                    <FeatureCard idx={2} selectedLang={selectedLang} />
+                                    <FeatureCard idx={2}
+                                        selectedLang={selectedLang} />
                                 </div>
                                 <div id="featureCard4">
-                                    <FeatureCard idx={4} selectedLang={selectedLang} />
+                                    <FeatureCard idx={4}
+                                        selectedLang={selectedLang} />
                                 </div>
                             </div>
                         </div>
